80d2d26073912eb1819324bc5360b9a89bdd906a
Boston Lowrey <bostonlowrey@gmail.com>
2023-07-12 12:12:39 -0400

    test: add more to the initial test

--- spec/requests/api/v1/merchant_spec.rb
+++ spec/requests/api/v1/merchant_spec.rb
@@ -7,6 +7,17 @@ RSpec.describe "Merchants API" do
 
       get '/api/v1/merchants'
       expect(response).to be_successful
+
+      merchants_data = JSON.parse(response.body, symbolize_names: true)
+      merchants = merchants_data[:data]
+      expect(merchants.count).to eq(10)
+
+      merchants.each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id].to_i).to be_an(Integer)
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_an(String)
+      end
     end
   end
 end
