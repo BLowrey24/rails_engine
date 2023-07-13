300de2784cf823ca390013d0d2b93b11d56e489c
Boston Lowrey <bostonlowrey@gmail.com>
2023-07-12 21:03:07 -0400

    test: add test for getting a merchant from item id

--- spec/requests/api/v1/merchant_item_request_spec.rb
+++ spec/requests/api/v1/merchant_item_request_spec.rb
@@ -24,4 +24,24 @@ RSpec.describe "Merchant Items API" do
       expect(item[:attributes][:name]).to be_a(String)
     end
   end
+
+  it "can get a merchant by an item" do
+    merchant1 = create(:merchant)
+    merchant2 = create(:merchant)
+    item = create(:item, merchant_id: merchant1.id)
+
+    get "/api/v1/items/#{item.id}/merchants"
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    merchant_data = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(merchant_data).to have_key(:id)
+    expect(merchant_data[:id].to_i).to eq(merchant1.id)
+    expect(merchant_data).to have_key(:type)
+    expect(merchant_data[:type]).to eq("merchant")
+    expect(merchant_data).to have_key(:attributes)
+    expect(merchant_data[:attributes]).to have_key(:name)
+    expect(merchant_data[:attributes][:name]).to eq(merchant1.name)
+  end
 end
