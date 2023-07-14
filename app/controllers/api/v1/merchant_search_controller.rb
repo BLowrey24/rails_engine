746ccf19021f91ba2582400ee917afd470d582bb
Boston Lowrey <bostonlowrey@gmail.com>
2023-07-13 23:30:54 -0400

    feat: add functionality for sad path in merchant search controller

--- app/controllers/api/v1/merchant_search_controller.rb
+++ app/controllers/api/v1/merchant_search_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::MerchantSearchController < ApplicationController
   def show
-    render json: MerchantSerializer.new(Merchant.merchant_search(params[:name]))
+    merchant = Merchant.merchant_search(params[:name])
+    if merchant.nil?
+      render json: ErrorSerializer.serialize("No merchant found with name #{params[:name]}."), status: 404
+    else
+      render json: MerchantSerializer.new(merchant)
+    end
   end
 end
