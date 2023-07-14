dd1158a8720241d182c23f06ce4ba8ace11e79b1
Boston Lowrey <bostonlowrey@gmail.com>
2023-07-13 23:25:08 -0400

    feat: add functionality for item_search request sad path

--- app/controllers/api/v1/item_search_controller.rb
+++ app/controllers/api/v1/item_search_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::ItemSearchController < ApplicationController
   def index
-    render json: ItemSerializer.new(Item.items_search(params[:name]))
+    items = Item.items_search(params[:name])
+    if items.empty?
+      render json: ErrorSerializer.serialize("No items found with name #{params[:name]}."), status: 404
+    else
+      render json: ItemSerializer.new(items)
+    end
   end
 end
